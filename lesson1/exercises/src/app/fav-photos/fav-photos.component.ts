c454f534755bcb02859b0d8ae25c2e27bc2ae9cf
ortegs04 <stevenmortega72@gmail.com>
2022-08-01 04:52:45 -0500

    Part 1 Changes

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi0AJ248kWRB9aUVQOJ5bEzr3Bgqfrtn3xbw&usqp=CAU';
+  image3 = 'https://www.meme-arsenal.com/memes/46af4bad3574b521c529676373030172.jpg';
 
   constructor() { }
 
--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['The Manchurian Candidate', 'Oceans 8', 'The Count of Monte Cristo', 'Lucky Number Sleven'];
 
    constructor() { }
 
